875c7f2d3400edb496961627a219aca90a4f72fe
Gurgii <michael.haggren@gmail.com>
2021-08-30 13:35:33 +0200

    Update

--- part2/countries/src/components/Countries.tsx
+++ part2/countries/src/components/Countries.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY);
 export type Country = {
   name: string;
   capital: string;
